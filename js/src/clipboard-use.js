59595d0e8f66e4005da16aaec90899b14b96042c
no5ix <no5ix@users.noreply.github.com>
2025-01-15 12:33:05 +0000

    deploy: 9f41a28396e55ca48fb58be6ab5b8591f3d0b07e

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -5,7 +5,7 @@
 	//   var copyHtml = '';
 	//   copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
 	// //   copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
-	//   copyHtml += '  <i class="fa fa-files-o" aria-hidden="true"></i>';
+	//   copyHtml += '  <i class="fa fa-copy" aria-hidden="true"></i>';
 	//   copyHtml += '</button>';
 	//   $(".highlight").wrap($('<div class="highlight-wrap"></div>'));
 	//   $('.highlight-wrap').prepend(copyHtml);
@@ -33,7 +33,7 @@
 		    // 如果代码有文件名的那一行, 原来top是7px, 会和文件名这一行重合, 所以要复制按钮往下移一点
 			copyHtml += ' style="top: 34px"';
 		copyHtml += '>';
-		copyHtml += '  <i class="fa fa-files-o" aria-hidden="true"></i>';
+		copyHtml += '  <i class="fa fa-copy" aria-hidden="true"></i>';
 		copyHtml += '</button>';
 		// $wrap.append($('<button>').addClass('btn-copy').append('复制').on('click', function (e) {
 		$wrap.append($(copyHtml).on('click', function (e) {
@@ -51,16 +51,20 @@
 			var result = document.execCommand('copy');
 			document.body.removeChild(ta);
 
-		   	// 点击了之后则复制按钮显示 "√", 800毫秒后恢复原样
+		   	// 点击了之后则复制按钮显示 "√"图标, 800毫秒后恢复原样
 			if(result) {
+				var $b = $wrap.find('.btn-copy')
 				// $(this).text('√');
 				// $(this).text('✓');
 				// $(this).text('✅');
-				$(this).text('✔');
+				// $(this).text('✔');
+				$b.find('i.fa-copy').remove();
+				$b.append($('<i class="fa fa-check" aria-hidden="true"></i>'));
 				setTimeout(function () {
-					var $b = $wrap.find('.btn-copy')
-					$b.text('');
-					$b.append($('<i class="fa fa-files-o" aria-hidden="true"></i>'));
+					// var $b = $wrap.find('.btn-copy')
+					// $b.text('');
+					$b.find('i.fa-check').remove();
+					$b.append($('<i class="fa fa-copy" aria-hidden="true"></i>'));
 				  }, 800);
 			}
 			// else $(this).text('复制失败')
@@ -71,7 +75,7 @@
 		//   var $b = $(this).find('.btn-copy')
 		//   setTimeout(function () {
 			// 	$b.text('');
-			// 	$b.append($('<i class="fa fa-files-o" aria-hidden="true"></i>'));
+			// 	$b.append($('<i class="fa fa-copy" aria-hidden="true"></i>'));
 		//   }, 300)
 		// })
 		.append(e);
